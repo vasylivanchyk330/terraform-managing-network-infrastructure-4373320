86075b1f3161f08401d34efe5f3b2f7bb3f9f473
vasylivanchyk330 <60069876+vasylivanchyk330@users.noreply.github.com>
2024-04-20 14:38:01 +0200

    revert fix

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
 
 variable "region" {
   type    = string
