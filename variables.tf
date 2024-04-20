b392eb2046317b8cb482dde2101e19ef9a0eeaec
vasylivanchyk330 <60069876+vasylivanchyk330@users.noreply.github.com>
2024-04-20 18:39:29 +0200

    fix try

--- variables.tf
+++ variables.tf
@@ -1,12 +1,4 @@
-variable "access_key" {
-  type      = string
-  sensitive = true
-}
 
-variable "secret_key" {
-  type      = string
-  sensitive = true
-}
 
 variable "region" {
   type    = string
