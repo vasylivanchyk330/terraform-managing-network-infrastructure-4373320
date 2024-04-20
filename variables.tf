7578ed1371e4119bea670eef0c60128ca7a9b73e
vasylivanchyk330 <60069876+vasylivanchyk330@users.noreply.github.com>
2024-04-20 18:37:20 +0200

    test

--- variables.tf
+++ variables.tf
@@ -1,13 +1,4 @@
 # security credential variables
-variable "access_key" {
-  type        = string
-  sensitive   = true
-}
-
-variable "secret_key" {
-  type        = string
-  sensitive   = true
-}
 
 variable "region" {
   type        = string
